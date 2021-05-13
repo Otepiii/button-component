596ca4156f6db97c506a755222d3399ba15fe294
joseph Evangelista <oootep123@gmail.com>
2021-05-12 22:13:36 -0700

    added variable and angular elements

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,17 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
 
-  @Input('startIcon') private startIcon: string;
+  public startIcon: string;
+
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-
 
 }
